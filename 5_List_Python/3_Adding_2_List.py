a9eacc50602b0d2164d67e9eed32d417497c5903
vpshubh <chakravartyshubh23@gmail.com>
2023-08-12 12:16:32 +0530

    nn

--- 5_List_Python/3_Adding_2_List.py
+++ 5_List_Python/3_Adding_2_List.py
@@ -5,13 +5,11 @@
 
 list_normal=[10,20,30,40,50]
 reversed_list = []
-# for data in range(len(list_normal)):
-#     #print("index:",data,"::",list_normal[data])
-#     temp=list_normal[data]
-#     list_normal[0]=list_normal[data-1]
-#     list_normal[data-1]=temp
-
 for i in range(1, len(list_normal)+1): 
     reversed_list.append(list_normal[-i])
 print(list_normal)
 print(reversed_list)
+
+third_list= list_normal+reversed_list
+print(third_list)
+
