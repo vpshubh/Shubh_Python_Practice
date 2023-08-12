8b5d33f5fa9219adf29d6c98f7713833bf0a1716
vpshubh <chakravartyshubh23@gmail.com>
2023-08-12 12:18:09 +0530

    bb

---
+++ 5_List_Python/3_Merging_2_List.py
@@ -0,0 +1,12 @@
+'''#WAP to merge two list in third list '''
+
+list_normal=[10,20,30,40,50]
+reversed_list = []
+for i in range(1, len(list_normal)+1): 
+    reversed_list.append(list_normal[-i])
+print(list_normal)
+print(reversed_list)
+
+third_list= list_normal+reversed_list
+print(third_list)
+
