1038870701e5b3bfe180cb04f7a700cecdf52c04
vpshubh <chakravartyshubh23@gmail.com>
2024-01-26 14:24:08 +0530

    ok

--- 3_For_Loop/Q26_Biggest_Digit_inNumber.py
+++ 3_For_Loop/Q26_Biggest_Digit_inNumber.py
@@ -2,13 +2,13 @@
 a=int(input("Enter the number\n"))
 
 # Convert the number to a string
-num_str = str(a)
+a_str = str(a)
 
 # Initialize the maximum digit with the first digit
-max_digit = int(num_str[0])
+max_digit = int(a_str[0])
 
 # Iterate through the remaining digits
-for digit_char in num_str[1:]:
+for digit_char in a_str[1:]:
     digit = int(digit_char)
     if digit > max_digit:
         max_digit = digit
